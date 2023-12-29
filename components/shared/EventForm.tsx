7e782acf07a5943feb8ebf9f057d8e2bccf662e7
Shammahkahangi <shammahkahangi@gmail.com>
2023-12-29 17:19:30 +0300

    Finish the app

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -19,7 +19,7 @@ import { useUploadThing } from '@/lib/uploadthing'
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox"
 import { useRouter } from "next/navigation"
-import { createEvent } from "@/lib/actions/event.actions"
+import { createEvent, updateEvent } from "@/lib/actions/event.actions"
 import { IEvent } from "@/lib/database/models/event.model"
 
 
@@ -84,20 +84,20 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
         return;
       }
 
-      // try {
-      //   const updatedEvent = await ({
-      //     userId,
-      //     event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
-      //     path: `/events/${eventId}`
-      //   })
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
+          path: `/events/${eventId}`
+        })
 
-      //   if(updatedEvent) {
-      //     form.reset();
-      //     router.push(`/events/${updatedEvent._id}`)
-      //   }
-      // } catch (error) {
-      //   console.log(error);
-      // }
+        if(updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`)
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
